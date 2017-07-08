51556fe59f24cacd2089bfb527b34b1ded990d55
EdG570 <cole570@hotmail.com>
2017-07-07 20:08:43 -0400

    Added express and mongodb connections setup

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const routes = require('./routes/routes');
+const app = express();
+
+mongoose.Promise = global.Promise;
+
+if (process.env.NODE_ENV !== 'test') {
+  mongoose.connect('mongodb://localhost/simplexity');
+  console.log('********* Connected to dev db ***********');
+}
+
+app.use(bodyParser.json());
+routes(app);
+
+app.use((err, req, res, next) => {
+  res.status(422).send({ error: err.message });
+});
+
+module.exports = app;
